2382001dc437d31a13c8f6799a8d0ea10ff4cef9
Conor-Spicer1 <spicer.conor@gmail.com>
2017-11-08 21:34:13 +0000

    Initial Commit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#Create directory NFL_stats
+git init
+git clone
+
+#Download & install Anaconda distribution
+# For Windows: https://conda.io/docs/user-guide/install/windows.html
+# For Mac OS: https://conda.io/docs/user-guide/install/macos.html
+conda create --name nfl_stats_env python=3.6
+source activate nfl_stats_env
